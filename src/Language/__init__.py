ad5de558d069b649d1ea75fd3ce8e00e4d34a65b
asistla <arvindsistla@gmail.com>
2018-10-15 19:57:29 +0530

    adding selenium drivers

--- src/Language/__init__.py
+++ src/Language/__init__.py
@@ -1,2 +1,6 @@
-from Language.crawl_data import getCsv
+from Language.crawl_data import getCsv, crawl
+import csv
+import datetime
 csvData = getCsv("E:\\hist.csv")
+htmldata=crawl()
+print htmldata
--- src/Language/crawl_data.py
+++ src/Language/crawl_data.py
@@ -1,6 +1,7 @@
-import csv
-import datetime
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import requests
 def getCsv(csvfile):
     with open(csvfile) as f:
         lines=f.readlines()
@@ -8,4 +9,13 @@ def getCsv(csvfile):
             lines[i]=lines[i].strip().split(',')
     return lines
     
-#def 
+def crawl():
+#    options = webdriver.ChromeOptions()
+#    options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\"
+#    driver=webdriver.Chrome(chrome_options=options)
+    driver=webdriver.Firefox()
+    url="https://in.investing.com/indices/msci-world-stock-historical-data"
+    driver.get(url)
+    elem=driver.find_element(By.ID, value='widgetFieldDateRange')
+    print elem
+    return requests.get(url, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
