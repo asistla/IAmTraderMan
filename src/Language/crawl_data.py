040e283f6b25f7015dcd1f17e79db715491562c7
asistla <arvindsistla@gmail.com>
2018-10-18 13:11:16 +0530

    additions

--- src/Language/crawl_data.py
+++ src/Language/crawl_data.py
@@ -1,21 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import requests
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 def getCsv(csvfile):
     with open(csvfile) as f:
         lines=f.readlines()
         for i in range(len(lines)):
             lines[i]=lines[i].strip().split(',')
     return lines
-    
+#[@class="popupCloseIcon largeBannerCloser"]
 def crawl():
-#    options = webdriver.ChromeOptions()
-#    options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\"
-#    driver=webdriver.Chrome(chrome_options=options)
     driver=webdriver.Firefox()
     url="https://in.investing.com/indices/msci-world-stock-historical-data"
     driver.get(url)
-    elem=driver.find_element(By.ID, value='widgetFieldDateRange')
+    killPopup=driver.find_element(By.XPATH, value='//*[@id="PromoteSignUpPopUp"]//*[@class="popupCloseIcon largeBannerCloser"]')
+    wait = WebDriverWait(killPopup, 30)
+    wait.until(EC.visibility_of(killPopup))
+    killPopup.click()
+    print killPopup
+    elem=driver.find_element(By.ID , value='widgetFieldDateRange')
     print elem
     return requests.get(url, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
